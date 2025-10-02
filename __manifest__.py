bec9a771c73db2c804651f59fafcfc1c4013dd1f
TRX <hectorquiroz@trixocom.com>
2025-10-02 13:47:48 -0300

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Git Module Installer',
-    'version': '18.0.1.0.0',
+    'version': '18.0.1.0.3',
     'category': 'Technical',
     'summary': 'Install Odoo modules directly from GitHub/GitLab repositories',
     'description': """
