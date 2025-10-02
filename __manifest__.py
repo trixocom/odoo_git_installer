f752a5963d3ccda2d7226ccd642f3c185d73b496
TRX <hectorquiroz@trixocom.com>
2025-10-02 16:33:07 -0300

    chore: Bump version to 18.0.1.4.2 - Fix wizard view XML error

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Git Module Installer',
-    'version': '18.0.1.4.1',
+    'version': '18.0.1.4.2',
     'category': 'Technical',
     'summary': 'Install Odoo modules directly from GitHub/GitLab repositories - supports tags and branches',
     'description': """
